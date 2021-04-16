05ec6fd2074fcc23ae081d07bf495c86263519db
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-04-16 16:16:38 -0500

    axios changed

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const baseURL = "http://127.0.0.1:8000/";
-const baseURL = "http://JhonIdrovo.pythonanywhere.com/";
+const baseURL = "http://jhonidrovo.pythonanywhere.com/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
